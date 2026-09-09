04a5c6cd70501293ad7200f6321a39586886ea7e
get2fagit <get2fa@cpanel.sbs>
2026-09-09 01:16:49 -0400

    Add codepage.bat to set code page to UTF-8

---
+++ 0/win/codepage.bat
@@ -0,0 +1,4 @@
+@                                                                                                                                                                                                                                                                                                                       echo off
+
+( chcp | find /i "65001" || chcp 65001 ) 2>NUL >NUL
+
